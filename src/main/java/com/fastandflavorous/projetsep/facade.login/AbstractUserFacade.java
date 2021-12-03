86a8980eb06d207e96fc21897c8cc05dc92ff6e9
Tupac <tupac.rocher@etu.umontpellier.fr>
2021-12-03 09:05:42 +0100

    Comments AbstractUserFactory and AbstractUserFacade

--- src/main/java/com/fastandflavorous/projetsep/facade.login/AbstractUserFacade.java
+++ src/main/java/com/fastandflavorous/projetsep/facade.login/AbstractUserFacade.java
@@ -19,17 +19,19 @@ public abstract class AbstractUserFacade {
     }
 
     /**
-     * @param token
-     * @return
+     * Ask the business logic to check the client credential
+     * @param token the token that allows the client to log in to the application
+     * @return It returns the client if the credential is right otherwise it returns the null
      */
     public Client checkClientLogin(String token) {
         return factory.checkClientLogin(token);
     }
 
     /**
-     * @param email
-     * @param password
-     * @return
+     * Ask the business logic to check the employee credentials
+     * @param email the email that allows the client to log in to the application
+     * @param password the password that allows the client to log in to the application
+     * @return It returns the employee if the credentials were right otherwise it returns the null
      */
     public Employee checkEmployeeLogin(String email, String password) {
         return factory.checkEmployeeLogin(email, password);
--- src/main/java/com/fastandflavorous/projetsep/factory.users/AbstractUserFactory.java
+++ src/main/java/com/fastandflavorous/projetsep/factory.users/AbstractUserFactory.java
@@ -24,6 +24,10 @@ public class AbstractUserFactory {
     private EmployeeManager employeeManager;
     private ClientManager clientManager;
 
+
+    private static final String SALT = "fastAndFlavorous";
+    private static final String ALGORITHM = "PBKDF2WithHmacSHA1";
+
     /**
      * Default constructor
      */
@@ -34,8 +38,9 @@ public class AbstractUserFactory {
     }
 
     /**
-     * @param token
-     * @return
+     * This method checks if the token received from the ui is corresponding to a token found by the DAO
+     * @param token the token that allows the client to log in to the application
+     * @return It returns the client if they are found by the DAO otherwise it returns the null
      */
     public Client checkClientLogin(String token) {
         Client client = dao.getClient(token);
@@ -46,27 +51,16 @@ public class AbstractUserFactory {
     }
 
     /**
-     * @param email
-     * @param password
-     * @return
+     * This method checks if the token received from the ui is corresponding to a token found by the DAO
+     * @param email the email that allows the client to log in to the application
+     * @param password the password that allows the client to log in to the application
+     * @return It returns the employee if they are found by the DAO otherwise it returns the null
      */
     public Employee checkEmployeeLogin(String email, String password) {
         Employee e = dao.getEmployee(email);
         if(e!=null) {
-            SecureRandom random = new SecureRandom();
-            byte[] salt = "fastAndFlavorous".getBytes();
-            KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
-            String base64encoded ="";
-            try{
-                SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-                byte[] hash = factory.generateSecret(spec).getEncoded();
-                base64encoded = Base64.getEncoder().encodeToString(hash);
-            }catch (NoSuchAlgorithmException| InvalidKeySpecException ex){
-                System.err.println(ex);
-
-            }
-
-            if (base64encoded.equals(e.getPassword())) {
+            String hashedEnteredPassword = getHashedPassword(password);
+            if (hashedEnteredPassword.equals(e.getPassword())) {
                 employeeManager.addEmployee(e);
                 return e;
             } else {
@@ -75,6 +69,26 @@ public class AbstractUserFactory {
         }else{
             return e;
         }
+    }
+
+
+    /**
+     * This methods hashes the password passed in parameter depending on the static constants SALT and ALGORITHM
+     * @param password password to hash
+     * @return The hashed password
+     */
+    private String getHashedPassword(String password){
+        byte[] salt = SALT.getBytes();
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), salt, 65536, 128);
+        String base64encoded ="";
+        try{
+            SecretKeyFactory factory = SecretKeyFactory.getInstance(ALGORITHM);
+            byte[] hash = factory.generateSecret(spec).getEncoded();
+            base64encoded = Base64.getEncoder().encodeToString(hash);
+        }catch (NoSuchAlgorithmException| InvalidKeySpecException ex){
+            System.err.println(ex);
+        }
+        return base64encoded;
     }
 
     /**
